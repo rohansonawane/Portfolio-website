c672ea62b788ea178aa61d3bafe0366d220899aa
Rohan Sonawane <rohan@Rohans-MacBook-Pro.local>
2025-06-04 00:42:05 -0700

    chore: remove Tawk.to chat widget

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,6 @@ import { JetBrains_Mono } from 'next/font/google'
 import './globals.css'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/next'
-import TawkToChat from "@/components/TawkToChat";
 import Script from 'next/script'
 
 // Components
@@ -113,7 +112,6 @@ export default function RootLayout({ children }) {
             {children}
           </PageTransition>
         </ErrorBoundary>
-        <TawkToChat />
         <Analytics />
         <SpeedInsights />
       </body>
