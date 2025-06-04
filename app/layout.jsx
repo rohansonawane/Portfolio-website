2b342ae73ff6bc01d757dceffb56728cc1e0665e
Rohan Sonawane <rohan@Rohans-MacBook-Pro.local>
2025-06-04 00:49:57 -0700

    fix: update favicon and icon configurations

--- app/layout.jsx
+++ app/layout.jsx
@@ -78,6 +78,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={jetbrainsMono.variable}>
       <head>
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="/favicon-16x16.png" type="image/png" sizes="16x16" />
+        <link rel="icon" href="/favicon-32x32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://fonts.googleapis.com" />
